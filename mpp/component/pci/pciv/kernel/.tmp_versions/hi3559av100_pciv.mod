7de310d07dc9f881464e9c0dbc4a4303424fcba1
PKUZHOU <751722308@qq.com>
2018-07-05 18:53:56 +0800

    delete

--- mpp/component/pci/pciv/kernel/.tmp_versions/hi3559av100_pciv.mod
+++
@@ -1,3 +0,0 @@
-/home/pub/himpp_git/himpp/board/mpp/component/pci/pciv/kernel/hi3559av100_pciv.ko
-/home/pub/himpp_git/himpp/board/mpp/component/pci/pciv/kernel/pciv.o /home/pub/himpp_git/himpp/board/mpp/component/pci/pciv/kernel/pciv_ext.o /home/pub/himpp_git/himpp/board/mpp/component/pci/pciv/kernel/pciv_drvadp.o
-
--- mpp/component/pci/pciv_fmw/.hi3559av100_pciv_fmw.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pub/himpp_git/himpp/board/mpp/component/pci/pciv_fmw/hi3559av100_pciv_fmw.o := aarch64-himix100-linux-ld -EL    -r -o /home/pub/himpp_git/himpp/board/mpp/component/pci/pciv_fmw/hi3559av100_pciv_fmw.o /home/pub/himpp_git/himpp/board/mpp/component/pci/pciv_fmw/pciv_firmware.o /home/pub/himpp_git/himpp/board/mpp/component/pci/pciv_fmw/pciv_pic_queue.o 
