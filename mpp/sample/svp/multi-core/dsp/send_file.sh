7a18a3edc579ea7a44ccb1084ed7b7a0b0862440
PKUZHOU <751722308@qq.com>
2018-07-05 18:55:15 +0800

    delete

--- mpp/sample/svp/multi-core/dsp/send_file.sh
+++
@@ -1,1 +0,0 @@
-scp sample_dsp_main zhouzhe@222.128.10.155:/media/2T/nfsroot/users/zhouzhe/dsp_new
--- mpp/sample/svp/multi-core/dsp/view_out.sh
+++
@@ -1,3 +0,0 @@
-scp zhouzhe@222.128.10.155:/media/2T/nfsroot/users/zhouzhe/dsp_new/src.jpg ./
-scp zhouzhe@222.128.10.155:/media/2T/nfsroot/users/zhouzhe/dsp_new/out.jpg ./
-feh out.jpg src.jpg
